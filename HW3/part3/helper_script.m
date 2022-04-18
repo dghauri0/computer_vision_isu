104bee6154626fd67b6b2e488e6705379afc0a19
DG1038286 <59807888+DG1038286@users.noreply.github.com>
2022-04-17 23:59:30 -0500

    helper script for opencv implementations

---
+++ HW3/part3/helper_script.m
@@ -0,0 +1,15 @@
+%% Your code goes in this file. 
+
+% Using this to get pixel RGB values 
+% (to use for deltas in C++ in coloring).
+main0 = imread('../part2/p2_image1.png');
+imshow(main0)
+impixelinfo()
+pause
+
+close all
+clc
+clear
+
+
+
