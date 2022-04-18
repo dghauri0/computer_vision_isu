eedb8c55b5221d83fe5b2089c58cd1b53096c2f6
DG1038286 <59807888+DG1038286@users.noreply.github.com>
2022-04-18 00:52:12 -0500

    part 3a complete

--- HW3/part3/p3a_code.cpp
+++ HW3/part3/p3a_code.cpp
@@ -1,1 +1,105 @@
-// Your code goes in this file. 
+// Your code goes in this file.
+
+// Headers
+#include <opencv2/core/core.hpp> 
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+#define R_LOW 200
+#define R_HIGH 255
+#define G_LOW 0
+#define G_HIGH 10
+#define B_LOW 0
+#define B_HIGH 10
+
+// compile command
+// cl /EHsc part3\p3a_code.cpp /I Y:\CodeLib\opencv\build\include /link /LIBPATH:Y:\CodeLib\opencv\build\x64\vc15\lib opencv_world455.lib
+
+
+int main(int argc, char* argv[]) {
+
+	// Load input video
+	//  If your video is in a different source folder than your code, 
+	//  make sure you specify the directory correctly!
+	VideoCapture input_cap("part3/p3_video2.avi");
+
+	// Check validity of target file
+	if (!input_cap.isOpened()) {
+		std::cout << "Input video not found." << std::endl;
+		return -1;
+	}
+
+	// Set up target output video
+	/*	usage: VideoWriter(filename, encoding, framerate, Size)
+	 *		in our case, cv_cap_prop_* means "get property of capture"
+	 *	 	we want our output to have the same properties as the input!
+	 */
+
+	VideoWriter output_cap("part3/p3a_video2_final.avi",
+		VideoWriter::fourcc('H', '2', '6', '4'),
+		input_cap.get(CAP_PROP_FPS),
+		Size(input_cap.get(CAP_PROP_FRAME_WIDTH),
+			input_cap.get(CAP_PROP_FRAME_HEIGHT)));
+
+	// Again, check validity of target output file
+	if (!output_cap.isOpened()) {
+		std::cout << "Could not create output file." << std::endl;
+		return -1;
+	}
+
+	// Loop to read from input one frame at a time, write text on frame, and
+	// copy to output video
+	Mat frame;
+	int count = 0;
+	while (input_cap.read(frame)) {
+		//cout << "working " << count << "\n";
+
+		Mat gray;
+		cvtColor(frame, gray, COLOR_BGR2GRAY);
+		medianBlur(gray, gray, 5);
+		vector<Vec3f> circles;
+		HoughCircles(gray, circles, HOUGH_GRADIENT, 1,
+			gray.rows / 32,
+			100, 30, 20, 40
+		);
+		for (size_t num = 0; num < circles.size(); num++) {
+			Vec3i c = circles[num];
+			Point center = Point(c[0], c[1]);
+
+			//circle(frame, center, 1, Scalar(255, 0, 0), 3, LINE_AA);
+			int radius = c[2];
+			
+
+			int x_coord = c[0];
+			int y_coord = c[1];
+			//printf("Here");
+			// rgb[0] is blue, rgb[1] is green, and rgb[2] is red
+			if (frame.at<Vec3b>(y_coord, x_coord)[0] >= 0 && frame.at<Vec3b>(y_coord, x_coord)[0] <= 20) {
+				if (frame.at<Vec3b>(y_coord, x_coord)[1] >= 0 && frame.at<Vec3b>(y_coord, x_coord)[1] <= 20) {
+					if (frame.at<Vec3b>(y_coord, x_coord)[2] >= 200 && frame.at<Vec3b>(y_coord, x_coord)[2] <= 255) {
+						
+						// color circle blue (as it has been identified as red with some delta)
+						//printf("Wow a red circle");
+						circle(frame, center, radius + 2, Scalar(255, 0, 0), -1, FILLED);
+					}
+				}
+			}
+			
+		}
+		count++;
+		output_cap.write(frame);
+	}
+
+
+	// free the capture objects from memory
+	input_cap.release();
+	output_cap.release();
+
+	return 1;
+
+}
