173c11d010f1caa2a4586d1fc64f3158b58bae26
DG1038286 <59807888+DG1038286@users.noreply.github.com>
2022-04-24 02:00:19 -0500

    helper script

---
+++ HW3/part4/helper_script.m
@@ -0,0 +1,15 @@
+%% Your code goes in this file. 
+
+% Using this to get pixel RGB values 
+% (to use for deltas in C++ in coloring).
+main0 = imread('../part3/helper_coord.png');
+imshow(main0)
+impixelinfo()
+pause
+
+close all
+clc
+clear
+
+
+
