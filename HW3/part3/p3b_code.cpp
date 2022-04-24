cd852e20ba3f2fc5c99ff0fdae65cfd0b3e361a2
DG1038286 <59807888+DG1038286@users.noreply.github.com>
2022-04-23 19:19:09 -0500

    p3b done

--- HW3/part3/p3b_code.cpp
+++ HW3/part3/p3b_code.cpp
@@ -17,6 +17,8 @@ using namespace std;
 #define G_HIGH 10
 #define B_LOW 0
 #define B_HIGH 10
+const int top_left_corner[2] = { 280, 526 };
+const int bottom_right_corner[2] = { 555, 595 };
 
 // compile command
 // cl /EHsc part3\p3b_code.cpp /I Y:\CodeLib\opencv\include /link /LIBPATH:Y:\CodeLib\opencv\x64\vc17\lib opencv_world455.lib
@@ -30,7 +32,7 @@ int main(int argc, char* argv[]) {
 	// Load input video
 	//  If your video is in a different source folder than your code, 
 	//  make sure you specify the directory correctly!
-	VideoCapture input_cap("part3/p3_video1.avi");
+	VideoCapture input_cap("part3/p3_video2.avi");
 
 	// Check validity of target file
 	if (!input_cap.isOpened()) {
@@ -44,7 +46,7 @@ int main(int argc, char* argv[]) {
 	 *	 	we want our output to have the same properties as the input!
 	 */
 
-	VideoWriter output_cap("part3/p3b_video1_final.avi",
+	VideoWriter output_cap("part3/p3b_video2_final.avi",
 		VideoWriter::fourcc('H', '2', '6', '4'),
 		input_cap.get(CAP_PROP_FPS),
 		Size(input_cap.get(CAP_PROP_FRAME_WIDTH),
@@ -76,15 +78,30 @@ int main(int argc, char* argv[]) {
 		split(frame, bgr);
 		//medianBlur(gray, gray, 15);
 		//cvtColor(gray, frame, COLOR_GRAY2BGR);
-		
+
 		// Thresholded Image (Binary Image)
 		threshold(gray, binaryImage, thresholdValue, MAX_BINARY, thresholdType);
 
-		
 
-		show_wait_destroy("Binary Image", binaryImage);
-		imwrite("part3/helper_coord.png", binaryImage);
 
+		//show_wait_destroy("Binary Image", binaryImage);
+		//imwrite("part3/helper_coord.png", binaryImage);
+		int i, j;
+		if (binaryImage.at<uint8_t>(top_left_corner[1], top_left_corner[0]) <= 20) {
+			for (i = top_left_corner[0]; i < bottom_right_corner[0]; i++) {
+				for (j = top_left_corner[1]; j < bottom_right_corner[1]; j++) {
+
+					if (binaryImage.at<uint8_t>(j, i) >= 200) {
+						frame.at<Vec3b>(j, i)[0] = 0;
+						frame.at<Vec3b>(j, i)[1] = 0;
+						frame.at<Vec3b>(j, i)[2] = 255;
+						printf("In loop ");
+					}
+				}
+			}
+		}
+
+		//show_wait_destroy("Binary Image Edited", binaryImage);
 
 		count++;
 		output_cap.write(frame);
