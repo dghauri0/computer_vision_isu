ae8e83c15e6ba5a73311e1e95351f845fd1768a0
DG1038286 <59807888+DG1038286@users.noreply.github.com>
2022-05-10 22:58:03 -0500

    almost there

--- HW5/part1_slow_forward.py
+++ HW5/part1_slow_forward.py
@@ -1,3 +1,4 @@
+import itertools
 import math
 
 # From lecture as check
@@ -8,8 +9,21 @@ B_ex = [[0.5, 0.0, 0.5],
 pi_ex = [1, 0]
 O_ex = [0, 2, 1]
 
+
 def slow_forward(A, B, pi, O):
-    L = 0
-    for state in range(len(pi)):
-        for time in range(len(O)):
-            
+    L = 0.0
+
+    for time_step_0 in range(len(pi)):
+        #mult = pi[time_step_0] * B[time_step_0][O[0]]
+        for time_step_1 in range(len(pi)):
+            #mult *= A[time_step_0][time_step_1] * B[time_step_1][O[1]]
+            for time_step_2 in range(len(pi)):
+                for time_step_3 in range(len(pi)):
+                mult = pi[time_step_0] * B[time_step_0][O[0]] * A[time_step_0][time_step_1] * B[time_step_1][O[1]] * A[time_step_1][time_step_2] * B[time_step_2][O[2]]
+                L += mult
+
+    return L
+
+
+temp = slow_forward(A_ex, B_ex, pi_ex, O_ex)
+print(temp)
--- HW5/partec1_forward_renormalization.py
+++ HW5/partec1_forward_renormalization.py
@@ -1,8 +1,20 @@
-import math
+import math, part2_forward_algorithm
+
+# From lecture as check
+A_ex = [[0.5, 0.5],
+        [0, 1]]
+B_ex = [[0.5, 0.0, 0.5],
+        [0, 1, 0]]
+pi_ex = [1, 0]
+O_ex = [0, 2, 1]
+
 
 def forward_algorithm_renorm(A, B, pi, O):
     forward_variable_array = [[0] * len(O) for i in range(len(pi))]
+    renorm_coeff = [0.0] * len(O)
+    print(renorm_coeff)
     for time in range(len(O)):
+        renorm_coeff[time] = 0
         for state in range(len(pi)):
             if time == 0:
                 forward_variable_array[state][time] = pi[state] * B[state][O[time]]
@@ -10,5 +22,16 @@ def forward_algorithm_renorm(A, B, pi, O):
                 for i in range(len(pi)):
                     forward_variable_array[state][time] += forward_variable_array[i][time - 1] * A[i][state] * B[state][
                         O[time]]
+            renorm_coeff[time] += forward_variable_array[state][time]
+        if renorm_coeff[time] == 0.0 or math.fabs(renorm_coeff[time]) < 1e-10:
+            renorm_coeff[time] = 1.0
+        else:
+            renorm_coeff[time] = 1.0 / renorm_coeff[time]
+            for k in range(len(pi)):
+                forward_variable_array[k][time] *= renorm_coeff[time]
 
     return forward_variable_array
+
+
+print(forward_algorithm_renorm(A_ex, B_ex, pi_ex, O_ex))
+print(part2_forward_algorithm.forward_algorithm(A_ex, B_ex, pi_ex, O_ex))
